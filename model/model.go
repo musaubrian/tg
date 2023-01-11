1c7fb2a56a8c0870c16d45de24a4c42622b1b363
musaubrian <musaubrian15@gmail.com>
2023-01-11 22:13:17 +0300

    chore: add model

--- model/model.go
+++ model/model.go
@@ -11,30 +11,26 @@ import (
 var db *gorm.DB
 
 // Site defines the structure of the db
-//
-// Name: name of the site
-// UserName: username for that site
-// Password: password for the site | TODO Should probably find a way to encrypt this
 type Site struct {
-    ID uint64 `json:"id" gorm:"primaryKey`
-    Name string `json:"name" gorm:"not null"`
-    UserName string `json:"user_name", gorm:"not null"`
-    Password string `json:"password", gorm:"not null"`
+	ID       uint64 `json:"id" gorm:"primaryKey`
+	Name     string `json:"name" gorm:"not null"`
+	UserName string `json:"user_name", gorm:"not null"`
+	Password string `json:"password", gorm:"not null"` // TODO Should probably find a way to encrypt this
 }
 
 // SetupDb creates a connection to the db
 // and initializes the table and columns
-func SetupDb(){
-    var err error
+func SetupDb() {
+	var err error
 
-    db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config)
-    if err != nil {
-        log.Fatal("Could not open db", err)
-        panic(err)
-    }
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config)
+	if err != nil {
+		log.Fatal("Could not open db", err)
+		panic(err)
+	}
 
-    err = db.AutoMigrate(&Site{})
-    if err != nil {
-        log.Fatal("Could not setup tables and stuff:", err)
-    }
+	err = db.AutoMigrate(&Site{})
+	if err != nil {
+		log.Fatal("Could not setup tables and stuff:", err)
+	}
 }
