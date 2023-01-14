ee1350e5d4b6705d0746fd5dfa0e6cea8741e57a
musaubrian <musaubrian15@gmail.com>
2023-01-14 16:18:21 +0300

    chore: create get ui working

--- model/model_handler.go
+++ model/model_handler.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Get input from the user
-func getInput(prompt string) string {
+func GetInput(prompt string) string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Printf("%s: ", prompt)
 
@@ -23,46 +23,44 @@ func getInput(prompt string) string {
 }
 
 // Adds a new site's details to the db
-func AddSite() {
+func AddSite(siteName string, username string, password string) {
 	var newSite Site
-	newSite.Name = getInput("Site Name")
-	newSite.UserName = getInput("Site's Username")
-	newSite.Password = getInput("Site's Password")
-	db.Create(&newSite)
-	fmt.Printf("\nSuccessfully added {%s}\n", newSite.Name)
+	newSite.Name = siteName
+	newSite.UserName = username
+	newSite.Password =password
+    db.Create(&newSite)
+//	fmt.Printf("\nSuccessfully added {%s}\n", newSite.Name)
 }
 
 // Updates the contents of a specified site
-func UpdateSite() {
+func UpdateSite(sitename string) {
 	var site Site
 
-	siteName := getInput("Site to update")
-	db.Where("name = ?", siteName).First(&site)
+	db.Where("name = ?", sitename).First(&site)
 	fmt.Println("\nEditing: ", site.Name)
 	fmt.Printf("Old details\nSiteName: {%s}  UserName {%s}  Password {%s}\n", site.Name, site.UserName, site.Password)
-	site.Name = getInput("New site name")
-	site.UserName = getInput("New userName")
-	site.Password = getInput("New Password")
+	site.Name = GetInput("New site name")
+	site.UserName = GetInput("New userName")
+	site.Password = GetInput("New Password")
 	db.Save(&site)
 	fmt.Println("\nUpdated Successfully")
 }
 
 // Delete records associated with a site
-func DeleteSite() {
-    var site Site
-    siteName := getInput("Site to delete")
-    db.Where("name = ?", siteName).Delete(&site)
-    fmt.Printf("\nDeleted {%s} successfully\n", siteName)
+func DeleteSite(sitename string) {
+	var site Site
+	db.Where("name = ?", sitename).Delete(&site)
+	fmt.Printf("\nDeleted {%s} successfully\n", sitename)
 }
 
 // SearchSite searches for a siteName that matches the users input
-func SearchSite() {
+func SearchSite(sitename string) {
 	var site Site
+
 	fmt.Println("//Searching")
-	name := getInput("Site name?")
-	result := db.Where("name = ?", name).First(&site)
+	result := db.Where("name = ?", sitename).First(&site)
 	if result.RowsAffected == 0 {
-		fmt.Println("No site found matching", name)
+		fmt.Println("No site found matching", sitename)
 	} else {
 		fmt.Println("\nSiteName:", site.Name)
 		fmt.Println("Username:", site.UserName)
@@ -85,18 +83,18 @@ func ListAll() {
 // Start of user interaction
 func TinyGo() {
 	fmt.Println("\n// Add[a] | Search[s] | Update[u] | Delete[d] | List all[l]\n")
-	startingPoint := getInput("What shall it be?")
+	startingPoint := GetInput("What shall it be?")
 	switch startingPoint {
 	case "a":
-		AddSite()
+	//	AddSite()
 	case "s":
-		SearchSite()
+	//	SearchSite()
 	case "d":
-        DeleteSite()
+	//	DeleteSite()
 	case "u":
-		UpdateSite()
+	//	UpdateSite()
 	case "l":
-		ListAll()
+	//	ListAll()
 	default:
 		fmt.Println("that option doesn't exist", startingPoint)
 	}
