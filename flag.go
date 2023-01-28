2872367e78171f9cbd558e77d2b87903e51de2ab
Ernest <musaubrian@users.noreply.github.com>
2023-01-28 12:05:24 +0000

    ci(format): format code using gofmt

--- flag.go
+++ flag.go
@@ -14,30 +14,30 @@ func CheckFlag() {
 
 	useGenerator := flag.Bool("p", false, "Generate password")
 	useDb := flag.Bool("d", false, "Access records")
-    help := flag.Bool("h", false, "Display usage instructions")
+	help := flag.Bool("h", false, "Display usage instructions")
 	flag.Parse()
 
-    switch {
-    case *useGenerator:
-        text := GeneratePassword()
-        fmt.Printf("\n%s\n", text)
-    case *useDb:
-        if err := CreateDir(); err != nil {
-            log.Fatal("Could not create directory", err)}
+	switch {
+	case *useGenerator:
+		text := GeneratePassword()
+		fmt.Printf("\n%s\n", text)
+	case *useDb:
+		if err := CreateDir(); err != nil {
+			log.Fatal("Could not create directory", err)
+		}
 
-
-        model.SetupDb()
-        model.TinyGo()
-    case *help:
-        usageInstructions()
-    default:
-        usageInstructions()
-    }
+		model.SetupDb()
+		model.TinyGo()
+	case *help:
+		usageInstructions()
+	default:
+		usageInstructions()
+	}
 }
 
 // Print usage instructions
-func usageInstructions()  {
-    fmt.Println(`
+func usageInstructions() {
+	fmt.Println(`
 Usage of tinygo:
     -d  Access records
     -h  Display usage instructions
