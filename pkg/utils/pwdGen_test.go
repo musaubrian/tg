52ed823691c29a3c4461d1fad1c0e6ed2c214efc
Ernest <musaubrian@users.noreply.github.com>
2023-02-09 12:28:14 +0000

    ci(format): format code using gofmt

--- pkg/utils/pwdGen_test.go
+++ pkg/utils/pwdGen_test.go
@@ -3,10 +3,10 @@ package utils
 import "testing"
 
 func TestGeneratePassword(t *testing.T) {
-    got := GeneratePassword()
-    want := 15
+	got := GeneratePassword()
+	want := 15
 
-    if len(got) != want {
-        t.Errorf("Got %q, want: %q", got, want)
-    }
+	if len(got) != want {
+		t.Errorf("Got %q, want: %q", got, want)
+	}
 }
