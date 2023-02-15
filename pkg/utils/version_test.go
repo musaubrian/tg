45b88b15969152c81eb5cca04ac1acba3422d96e
Ernest <musaubrian15@gmail.com>
2023-02-15 17:36:12 +0300

    test(utils): add tests for GetVersion

--- pkg/utils/version_test.go
+++ pkg/utils/version_test.go
@@ -7,12 +7,15 @@ import (
 
 
 func TestGetVersion(t *testing.T) {
-    got := GetVersion()
+    got, err := GetVersion()
     if len(got) < 0 {
         t.Error("expected a non empty string got an empty string")
     }
 
     if strings.Contains(got, "v") != true {
         t.Error("Expected true as the tags are prefixed with 'v', got false")
+    }
+    if err != nil {
+        t.Error("Expected no error returned, got an error")
     }
 }
