5eda244ffb5204bc857b6f2dc7cea45701cafc7a
musaubrian <musaubrian15@gmail.com>
2023-10-10 15:55:59 +0300

    feat: implement copy to clipboard option

--- cmd/pwd.go
+++ cmd/pwd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/musaubrian/tinygo/internal/utils"
 	"github.com/spf13/cobra"
@@ -12,10 +13,22 @@ var pwdCmd = &cobra.Command{
 	Use:     "pwd",
 	Short:   "Generate a 15 character string as password",
 	Aliases: []string{"p"},
-	// Long: ``
+	Long: `
+Generate a 15 character string as password
+parse 'c' as argument to copy it without displaying it
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		pwd := utils.GeneratePassword()
-		fmt.Println(pwd)
+		if len(args) < 1 {
+			fmt.Println(pwd)
+		} else {
+			if args[0] == "c" {
+				utils.CopyToClipboard(pwd)
+				fmt.Println("Copied to clipboard")
+			}else {
+				log.Fatalf("Run `%s pwd -h` to see how to use this command", rootCmd.Use)
+			}
+		}
 	},
 }
 
